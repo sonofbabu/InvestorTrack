1da8c0d098ac41cd8bcf5da4bb7644185ef290e3
leyo babu <leyo@oustlabs.com>
2020-11-16 17:40:11 +0530

    Initial Commit

---
+++ Inverstor TrackTests/Inverstor_TrackTests.swift
@@ -0,0 +1,34 @@
+//
+//  Inverstor_TrackTests.swift
+//  Inverstor TrackTests
+//
+//  Created by leyo babu on 16/11/20.
+//  Copyright © 2020 leyo babu. All rights reserved.
+//
+
+import XCTest
+@testable import Inverstor_Track
+
+class Inverstor_TrackTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
